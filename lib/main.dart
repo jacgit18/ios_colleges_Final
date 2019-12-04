bad10ad84b5f7a7f6ea917129e331507f372ef68
jacgit18 <joshuacarpentier21@gmail.com>
2019-12-04 16:17:04 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix0;
 import 'colleges.dart';
 
 void main() => runApp(MyApp());
@@ -82,31 +81,31 @@ class _MyHomePageState extends State<MyHomePage> {
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  CollegeButton(collegename:'WSU', collegewidget: Wsu(),color1: Colors.black, color2: Colors.yellow,),
-                  CollegeButton(collegename:'OU', collegewidget:Ou(), color1: const Color(0xFF750b10), color2: Colors.white,),
-                  CollegeButton(collegename:'OSU', collegewidget: Osu(), color1: Colors.orange, color2: Colors.black,),
-                  CollegeButton(collegename:'KU', collegewidget: Ku(), color1: const Color(0xFF2956B5), color2: Colors.white,),
-                  CollegeButton(collegename:'MIT', collegewidget: Mit(), color1: const Color(0xFF8A8B8C), color2: const Color(0xFFA31F34),),
+                  CollegeButton(collegename:'WSU', collegewidget: Wsu()),
+                  CollegeButton(collegename:'OU', collegewidget:Ou()),
+                  CollegeButton(collegename:'OSU', collegewidget: Osu()),
+                  CollegeButton(collegename:'KU', collegewidget: Ku()),
+                  CollegeButton(collegename:'MIT', collegewidget: Mit()),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  CollegeButton(collegename:'CUNY', collegewidget:Cuny(), color1: const Color(0xFF1D3A83), color2: Colors.white,),
-                  CollegeButton(collegename:'NYU', collegewidget: Nyu(), color1: const Color(0xFF57068C), color2: Colors.white,),
-                  CollegeButton(collegename:'OPSU', collegewidget: Opsu(), color1: Colors.red, color2: Colors.black,),
-                  CollegeButton(collegename:'RICE', collegewidget: Rice(), color1: const Color(0xFF00205B), color2: const Color(0xFFC1C6C8),),
-                  CollegeButton(collegename:'BROWN', collegewidget: Brown(), color1: Colors.red, color2: Colors.white,),
+                  CollegeButton(collegename:'CUNY', collegewidget:Cuny()),
+                  CollegeButton(collegename:'NYU', collegewidget: Nyu()),
+                  CollegeButton(collegename:'OPSU', collegewidget: Opsu()),
+                  CollegeButton(collegename:'RICE', collegewidget: Rice()),
+                  CollegeButton(collegename:'BROWN', collegewidget: Brown()),
                 ],
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  CollegeButton(collegename:'ISU', collegewidget: Isu(),color1: Colors.black, color2: Colors.white,),
-                  CollegeButton(collegename:'DUKE',collegewidget: Duke(),color1: const Color(0xFF00006c), color2: Colors.white,),
-                  CollegeButton(collegename:'UTULSA', collegewidget: Utulsa(),color1: const Color(0xFFE0CE78), color2: const Color(0xFF003366),),
-                  CollegeButton(collegename:'BC', collegewidget: Bc(),color1: const Color(0xFF002145), color2: Colors.white,),
-                  CollegeButton(collegename:'SUNY', collegewidget: Suny(),color1:  const Color(0xFF009EE0), color2:  const Color(0xFF004C93),),
+                  CollegeButton(collegename:'ISU', collegewidget: Isu()),
+                  CollegeButton(collegename:'DUKE',collegewidget: Duke()),
+                  CollegeButton(collegename:'UTULSA', collegewidget: Utulsa()),
+                  CollegeButton(collegename:'BC', collegewidget: BC()),
+                  CollegeButton(collegename:'SUNY', collegewidget: Suny()),
                 ],
               )
             ]),
@@ -120,16 +119,15 @@ class CollegeButton extends StatelessWidget{
   final String collegename;
   final Color color1;
   final Widget collegewidget;
-  final Color color2;
 
-  CollegeButton({this.collegename, this.color1 = Colors.lightBlue, this.collegewidget, this.color2 = Colors.yellow});
+  CollegeButton({this.collegename, this.color1 = Colors.lightBlue, this.collegewidget});
 
   @override
   Widget build(BuildContext context) {
 
-    return  FlatButton(
-       color: color1,
-      child: Text(this.collegename,style: TextStyle(color: color2),),
+    return FlatButton(
+      color: Colors.purple,
+      child: Text(this.collegename),
         onPressed: () => Navigator.push(
             context,
             MaterialPageRoute(
