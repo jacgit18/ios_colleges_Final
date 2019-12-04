f196884a32cb1ed6f5e95ba86139603f759332b5
jacgit18 <joshuacarpentier21@gmail.com>
2019-12-04 16:16:09 -0500

    Update colleges.dart

--- lib/colleges.dart
+++ lib/colleges.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:ios_colleges_with_images/cities.dart';
 
+
 class College extends StatelessWidget{
+
   final String collegename;
   final String collegetext;
   final String citytext;
   final String collegelogo;
   final Widget citywidget;
   final Color color1;
-  College({this.collegename,this.collegetext,this.citytext,this.collegelogo,this.citywidget
-    ,this.color1=Colors.lightGreen});
+  College({this.collegename,this.collegetext,this.citytext,this.collegelogo,this.citywidget,this.color1});
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home:Scaffold(
         appBar: AppBar(
-          title: Text(collegename),
+          title: Text('collegename'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -26,7 +28,6 @@ class College extends StatelessWidget{
             Image.asset(collegelogo),
 
             Text(collegetext),
-
             FlatButton(color: color1 ,onPressed: () =>
                 Navigator.push(context,
                     MaterialPageRoute(builder: (BuildContext context) => citywidget)
@@ -42,162 +43,169 @@ class College extends StatelessWidget{
 
 class Wsu extends College{
   Wsu():super(
-      collegename: '.Wichita State University',
-      collegetext: 'Tuition costs around 12000 a year for its computer science program.',
-      citytext: 'City of Wichita',
-      collegelogo: 'assets/Wsu.png',
-      citywidget: Wichita()
+    collegename:'Witcha State University',
+    collegetext: ' Tuition cost around 12000 a year for its computer science , '
+        'it is located in Witcha, Kansas',
+    citytext: 'City of Witchata',
+    collegelogo:'assets/Wsu.png',
+    citywidget: Wichita(),
   );
 }
 
 class Ou extends College{
   Ou():super(
-      collegename: 'University of Oklahoma',
-      collegetext: 'Tuition costs around 15000 a year for its computer science program. '
-          'It is located in Norman, Oklahoma',
-      citytext: 'City of Norman',
-      collegelogo: 'assets/Ou.jpg',
-      citywidget: Norman()
+    collegename:'The University of Oklahoma',
+    collegetext: 'Tuition cost around 15000 a year for its computer science , it is located in norman, Oklohoma',
+    citytext:'City of Norman',
+    collegelogo:'assets/Ou.jpg',
+    citywidget: Norman(),
   );
 }
-
 class Osu extends College{
   Osu():super(
-      collegename: 'Oklahoma State University',
-      collegetext: 'Tuition costs around 15000 a year for its computer science program. '
-          'It is located in Stilwater, Oklahoma',
-      citytext: 'City of Stillwater',
-      collegelogo: 'assets/Osu.jpg',
-      citywidget: Stillwater()
+    collegename:'Ohio State University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of stillwater',
+    collegelogo:'assets/Osu.jpg',
+    citywidget: Stillwater(),
   );
 }
-
 
 class Ku extends College{
   Ku():super(
-      collegename: 'University of kansas',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. '
-          'It is located in lawrence, Kansas',
-      citytext: 'City of Lawrence',
-      collegelogo: 'assets/Ku.jpg',
-      citywidget: Lawrence()
+    collegename:'University of Kansas',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of Kansas',
+    collegelogo:'assets/Ku.jpg',
+    citywidget: Lawrence(),
   );
 }
-
 
 class Mit extends College{
   Mit():super(
-      collegename: 'Massachussets Institute of Technology',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. '
-          'It is located in Cambridge, Massachussets',
-      citytext: 'City of Cambridge',
-      collegelogo: 'assets/Mit.jpg',
-      citywidget: Cambridge()
+    collegename:'University of Kansas',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of Cambridge',
+    collegelogo:'assets/Mit.jpg',
+    citywidget: Cambridge(),
   );
 }
+
 
 class Cuny extends College{
   Cuny():super(
-      collegename: 'City University of New York',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. '
-          'It is located in Brooklyn, NY',
-      citytext: 'City of Brooklyn',
-      collegelogo: 'assets/cuny.png',
-      citywidget: Brooklyn()
+    collegename:'City University of New York',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' Bouroughs of New York',
+    collegelogo:'assets/cuny.png',
+    citywidget: Cambridge(),
   );
 }
-
 
 class Nyu extends College{
   Nyu():super(
-      collegename: 'New York University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. '
-          'It is located in Brooklyn, NY',
-      citytext: 'City of Brooklyn',
-      collegelogo: 'assets/Nyu.jpg',
-      citywidget: Manhattan()
+    collegename:'New York University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' Bourough of Manhattan',
+    collegelogo:'assets/Nyu.jpg',
+    citywidget: Manhattan(),
   );
 }
 
+
 class Opsu extends College{
   Opsu():super(
-      collegename: 'Oklahoma Panhandle State University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. ',
-      citytext: 'City of Goodwell',
-      collegelogo: 'assets/opsu.png',
-      citywidget: Goodwell()
+    collegename:'Oklahoma Panhandle State University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:'Goodwell',
+    collegelogo:'assets/opsu.png',
+    citywidget: Goodwell(),
   );
 }
 
 class Rice extends College{
   Rice():super(
-      collegename: 'RICE University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. ',
-      citytext: 'City of Houston',
-      collegelogo: 'assets/Rice.png',
-      citywidget: Houston()
+    collegename:'Rice University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:'Houston',
+    collegelogo:'assets/Rice.png',
+    citywidget: Houston(),
   );
 }
 
 class Brown extends College{
   Brown():super(
-      collegename: 'BROWN University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. ',
-      citytext: 'City of Providence',
-      collegelogo: 'assets/Brown.jpg',
-      citywidget: Providence()
+    collegename:'Brown University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of Providence',
+    collegelogo:'assets/Brown.jpg',
+    citywidget: Providence(),
   );
 }
 
 class Isu extends College{
   Isu():super(
-      collegename: 'Idaho State University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. ',
-      citytext: 'Illinois State University',
-      collegelogo: 'assets/illinois.jpg',
-      citywidget: Normal()
+    collegename:'Illinois State University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of Normal, Illinois',
+    collegelogo:'assets/illinois.jpg',
+    citywidget: Normal(),
   );
 }
 
 class Duke extends College{
   Duke():super(
-      collegename: 'Duke University',
-      collegetext: 'Tuition costs around 15600 a year for its computer science program. ',
-      citytext: 'City of Durham',
-      collegelogo: 'assets/Duke.jpg',
-      citywidget: Carolina()
+    collegename:'Duke University',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of North Carolina',
+    collegelogo:'assets/Duke.jpg',
+    citywidget: Carolina(),
   );
 }
+
 
 class Utulsa extends College{
   Utulsa():super(
-      collegename: 'Utulsa University',
-      collegetext: 'Tuition is 15000 a year for computer science. It is located in Tulsa, OKlahoma.',
-      citytext: 'City of Tulsa',
-      collegelogo: 'assets/utulsa.jpg',
-      citywidget: Tulsa()
-  );
-}
-class Bc extends College{
-  Bc():super(
-      collegename: 'British Columbia',
-      collegetext: 'Tuition is 15000 a year for computer science. It is located in Vancouver, BC.',
-      citytext: 'City of Vancouver',
-      collegelogo: 'assets/cambridge.jpg',
-      citywidget: Vancouver()
+    collegename:'The University of Tulsa',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:' City of Tulsa, Oklahoma',
+    collegelogo:'assets/utulsa.jpg',
+    citywidget: Tulsa(),
   );
 }
 
-//Reginald Metellus
+class BC extends College{
+  BC():super(
+    collegename:'State University of New York',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:'Albany',
+    collegelogo:'assets/SUNY.jpg',
+    citywidget: Vancouver(),
+  );
+}
 
 class Suny extends College{
   Suny():super(
-      collegename: 'State University of New York',
-      collegetext: 'Tuition is 29000 a year for computer science. It is located in Albany, Ny',
-      citytext: 'City of Albany',
-      collegelogo: 'assets/SUNY.jpg',
-      citywidget: Albany()
+    collegename:'State University of New York',
+    collegetext: ' Tuition cost around 15000 a year for its computer science , '
+        'it is located in norman, Oklohoma',
+    citytext:'Albany',
+    collegelogo:'assets/SUNY.jpg',
+    citywidget: Albany(),
   );
 }
+
 
 
