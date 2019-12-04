b72d886783447fad2363e72d4f8f53bdb074675b
jac <joshuacarpentier21@gmail.com>
2019-12-04 03:06:24 -0500

    Test push

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Joshua Carpentier '),
+      home: MyHomePage(title: 'Joshua Carpentier'),
     );
   }
 }
