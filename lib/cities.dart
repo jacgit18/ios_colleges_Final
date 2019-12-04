5d0171b88364e4f548f7f18fcac2aacdec77ec62
jacgit18 <joshuacarpentier21@gmail.com>
2019-12-04 16:46:41 -0500

    Update cities.dart

--- lib/cities.dart
+++ lib/cities.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ios_colleges_with_images/states.dart';
-import 'dart:math';
 
 class Wichita extends StatelessWidget {
-  final fish2 = Distance();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,30 +15,8 @@ class Wichita extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-                Image.asset('assets/Wichita1.jpg'),
-                SizedBox(height: 5.0),
-            Text(" Wichita is greatest place ahhsfh fhs fhs fh shf shfhj fhjs fhs ffsonfu"
-                "fjfjskfjnskjfnksjnfkjs "),
 
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Kansas())
-            ),
-
-              child: Text("Kansas"),
-            ),
-            Text("distance: " + fish2.calc(
-
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 37.66,
-                destinationy: -97.72
-            ).toString() + "Km. " + "or " + (fish2.calc(
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 37.66,
-                destinationy: -97.72
-            ) * 0.62).toString() + " in miles. ")
-
+            Text(" Wichita is "),
           ],
         ),
       ),
@@ -70,15 +44,8 @@ class Norman extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/Norman2.jpg'),
-            SizedBox(height: 5.0),
-            Text(" norman is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Oklahoma())
-            ),
 
-              child: Text("Oklahoma"),
-            ),
+            Text(" norman is "),
           ],
         ),
       ),
@@ -105,15 +72,8 @@ class Stillwater extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/Stillwater1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" stillwater is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Oklahoma())
-            ),
 
-              child: Text("Oklahoma"),
-            ),
+            Text(" stillwater is "),
           ],
         ),
       ),
@@ -141,15 +101,8 @@ class Lawrence extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/Lawerance1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Lawrence is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Kansas())
-            ),
 
-              child: Text("Kansas"),
-            ),
+            Text(" Lawrence is "),
           ],
         ),
       ),
@@ -176,16 +129,8 @@ class Cambridge extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/masscam1.jpg'),
-            SizedBox(height: 5.0),
+
             Text(" Cambridge is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Massachusetts())
-            ),
-
-              child: Text("Massachusetts"),
-            ),
-
           ],
         ),
       ),
@@ -210,15 +155,8 @@ class Brooklyn extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/brooklyn1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Bouroughs of New York is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => NewYork())
-            ),
 
-              child: Text("New York"),
-            ),
+            Text(" Bouroughs of New York is "),
           ],
         ),
       ),
@@ -243,15 +181,8 @@ class Manhattan extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/Manhattan1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Manhattan is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => NewYork())
-            ),
 
-              child: Text("New York"),
-            ),
+            Text(" Manhattan is "),
           ],
         ),
       ),
@@ -276,15 +207,8 @@ class Goodwell extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.lightGreen, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/goodwell1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Goodwell is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Oklahoma())
-            ),
 
-              child: Text("Oklahoma"),
-            ),
+            Text(" Goodwell is "),
           ],
         ),
       ),
@@ -310,15 +234,8 @@ class Goodman extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/masscam1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Goodman is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Oklahoma())
-            ),
 
-              child: Text("Oklahoma"),
-            ),
+            Text(" Goodman is "),
           ],
         ),
       ),
@@ -344,15 +261,8 @@ class Houston extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/Texas1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Houston is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Texas())
-            ),
 
-              child: Text("Texas"),
-            ),
+            Text(" Houston is "),
           ],
         ),
       ),
@@ -377,15 +287,8 @@ class Providence extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/prov1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Rhode Island is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => RhodeIsland())
-            ),
 
-              child: Text("Rhode Island"),
-            ),
+            Text(" Rhode Island is "),
           ],
         ),
       ),
@@ -410,15 +313,8 @@ class Normal extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/normill1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" Normal, Illinois is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Illinois())
-            ),
 
-              child: Text("Illinois"),
-            ),
+            Text(" Normal, Illinois is "),
           ],
         ),
       ),
@@ -429,7 +325,7 @@ class Normal extends StatelessWidget {
 }
 
 
-class Carolina extends StatelessWidget {
+class  Carolina extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -444,15 +340,8 @@ class Carolina extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/North-Carolina1.jpg'),
-            SizedBox(height: 5.0),
-            Text(" North Carolina is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => NorthCarolina())
-            ),
 
-              child: Text("North Carolina"),
-            ),
+            Text(" North Carolina is "),
           ],
         ),
       ),
@@ -463,8 +352,6 @@ class Carolina extends StatelessWidget {
 }
 
 class  Tulsa extends StatelessWidget {
-  final fish2 = Distance();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -479,27 +366,8 @@ class  Tulsa extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/tulsa1.JPG'),
-            SizedBox(height: 5.0),
+
             Text(" Tulsa, Oklahoma is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Oklahoma())
-            ),
-
-              child: Text("Oklahoma"),
-            ),
-            Text("distance: " + fish2.calc(
-
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 35.48,
-                destinationy: -97.61
-            ).toString() + "Km. " + "or " + (fish2.calc(
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 35.48,
-                destinationy: -97.61
-            ) * 0.62).toString() + " in miles. ")
           ],
         ),
       ),
@@ -510,8 +378,6 @@ class  Tulsa extends StatelessWidget {
 }
 
 class Vancouver extends StatelessWidget {
-  final fish2 = Distance();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -526,28 +392,8 @@ class Vancouver extends StatelessWidget {
           children: <Widget>[
             FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
                 child: Text("Go back")),
-            Image.asset('assets/britsh1.jpg'),
-            SizedBox(height: 5.0),
+
             Text(" Vancouver is "),
-            FlatButton(color: Colors.green, onPressed: ()=> Navigator.push(
-                context, MaterialPageRoute(builder: (BuildContext context) => Canada())
-            ),
-
-              child: Text("Canada"),
-
-            ),
-            Text("distance: " + fish2.calc(
-
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 42.66,
-                destinationy: -73.88
-            ).toString() + "Km. " + "or " + (fish2.calc(
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 42.66,
-                destinationy: -73.88
-            ) * 0.62).toString() + " in miles. ")
           ],
         ),
       ),
@@ -558,142 +404,27 @@ class Vancouver extends StatelessWidget {
 }
 
 class Albany extends StatelessWidget {
-  final capitol = LandMarks(title: 'State Capitol',
-      description: 'The New York State Capitol, the seat of the New York State government, '
-          'is located in Albany, the capital city of the U.S. state of New York. '
-          'The capitol building is part of the Empire State Plaza complex on State Street in Capitol Park.'
-          ' Housing the New York State Legislature, '
-          'the building was completed in 1899 at a cost of US\$25 million '
-          '(equivalent to \$753 million in 2018), '
-          'making it the most expensive government building of its time.[3] '
-          'It was listed on the National Register of Historic Places in 1971',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/4/42/NYSCapitolPanorama.jpg');
-  final museum = LandMarks(title: 'The New York State Museum',
-      description: 'The New York State Museum is a research-backed institution in Albany, New York, United States. '
-          'It is located on Madison Avenue, attached to the south side of the Empire State Plaza, '
-          'facing onto the plaza and towards the New York State Capitol. The museum houses art, '
-          'artifacts (prehistoric and historic), and ecofacts that reflect New York’s cultural, natural, '
-          'and geological development. Operated by the New York State Education Department\'s'
-          'Office of Cultural Education, it is the nation\'s oldest and largest state museum.',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/c1/NewYorkStateCulturalEducationCenter.JPG');
-  final List<LandMarks> albanylandmarks = [];
-  final TrackIt fish = TrackIt();
-  final fish2 = Distance(
-
-      originx: 2,
-      originy: 11,
-      destinationx: 5,
-      destinationy: 7
-
-  );
-
   @override
   Widget build(BuildContext context) {
-
-    if (fish.fish()) {
-      albanylandmarks.add(capitol);
-      albanylandmarks.add(museum);
-    }
-
-    fish2.calc(
-
-        originx: 40.64,
-        originy: -74.01,
-        destinationx: 42.66,
-        destinationy: -73.88
-    );
-
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('City of Albany'),),
-        body: Column(
+          title: Text("Albany, New York "),
+
+
+        ),
+        body:Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            FlatButton(color: Colors.lightBlue,
-                onPressed: () => Navigator.pop(context),
-                child: Text('Go Back')),
-            Image.asset('assets/albany1.jpg'),
-            Text('Albany is a city in Ny.'
-                '......'),
-            Expanded(child: ListView.builder(
-              itemCount: albanylandmarks.length,
-              itemBuilder: (BuildContext ctxt, final int index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        albanylandmarks[index].image),
-                  ),
-                  title: Text(albanylandmarks[index].title,),
-                  subtitle: Text(albanylandmarks[index].description),
-                );
-              },
-            ),),
-            FlatButton(color: Colors.deepPurple, onPressed: () =>
-                Navigator.push(
-                    context, MaterialPageRoute(
-                    builder: (BuildContext context) => NewYork())
-                ),
-                child: Text('New York')),
-            Text("distance: " + fish2.calc(
+            FlatButton(color: Colors.deepPurple, onPressed: ()=> Navigator.pop(context) ,
+                child: Text("Go back")),
 
-                originx: 40.64,
-                originy: -74.01,
-                destinationx: 42.66,
-                destinationy: -73.88
-            ).toString() + "Km. " + "or " + (fish2.calc(
-                    originx: 40.64,
-                    originy: -74.01,
-                    destinationx: 42.66,
-                    destinationy: -73.88
-                ) * 0.62).toString() + " in miles. ")
+            Text(" Albany is "),
           ],
         ),
       ),
+
+
     );
-  }
-}
-
-
-
-class TrackIt{
-  int bird =0;
-  TrackIt();
-  bool fish(){
-    if (bird==0){
-      print("bird: "+ bird.toString());
-      bird= bird+1;
-      return true;
-    }
-    else{
-      print("bird: "+ bird.toString());
-      return false;
-    }
-  }
-}
-
-
-class LandMarks{
-  final String title;
-  final String description;
-  final String image;
-  LandMarks({this.title,this.description,this.image});
-}
-
-
-class Distance {
-  double originx, originy, destinationx, destinationy;
-  double result, x, y, deglen = 110.25;
-
-  Distance({this.originx = 5.0, this.originy = 11.0, this.destinationx = 5.0,
-    this.destinationy = 7.0});
-
-  double calc({originx, originy, destinationx, destinationy }){
-    x = originx - destinationx;
-    y = (originy - destinationy) * cos(destinationx);
-    result = (deglen * sqrt(x*x + y*y)).floorToDouble();
-    print('distance is: ' + result.toString());
-    return result;
   }
 }
